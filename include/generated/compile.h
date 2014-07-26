a01f82bac3dc364caadda795e802826d1e65cd5b
“PriyankaSingh” <priancasingh1@gmail.com>
2014-07-26 20:46:11 +0530

    NEKTech-Linux1.0 Stock Kernel Source Code.

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#1 SMP Fri Mar 28 20:38:27 IST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "nektech-Latitude-E6400"
+#define LINUX_COMPILER "gcc version 4.6.3 (Ubuntu/Linaro 4.6.3-1ubuntu5) "
